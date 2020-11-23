ff551bfa9949caca8df9c6b65ef499ebbc6bc80e
Bruno de Renzis <brunoderenzisrubinetti26@gmail.com>
2020-11-22 23:46:59 -0300

    56% del parcial hecho}

---
+++ 2Parcial/FormEnvios/FormPpal.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace FormEnvios
+{
+    partial class FormPpal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTxtPrepEnvio = new System.Windows.Forms.RichTextBox();
+            this.richTextDelivery = new System.Windows.Forms.RichTextBox();
+            this.richTextRecibido = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTxtEnvio = new System.Windows.Forms.Label();
+            this.richTxtEntregado = new System.Windows.Forms.Label();
+            this.lblRetiro = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxPrepRetiro = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pedidoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTxtPrepEnvio
+            // 
+            this.richTxtPrepEnvio.Location = new System.Drawing.Point(115, 74);
+            this.richTxtPrepEnvio.Name = "richTxtPrepEnvio";
+            this.richTxtPrepEnvio.Size = new System.Drawing.Size(338, 310);
+            this.richTxtPrepEnvio.TabIndex = 0;
+            this.richTxtPrepEnvio.Text = "";
+            // 
+            // richTextDelivery
+            // 
+            this.richTextDelivery.Location = new System.Drawing.Point(651, 74);
+            this.richTextDelivery.Name = "richTextDelivery";
+            this.richTextDelivery.Size = new System.Drawing.Size(338, 310);
+            this.richTextDelivery.TabIndex = 1;
+            this.richTextDelivery.Text = "";
+            // 
+            // richTextRecibido
+            // 
+            this.richTextRecibido.Location = new System.Drawing.Point(1167, 74);
+            this.richTextRecibido.Name = "richTextRecibido";
+            this.richTextRecibido.Size = new System.Drawing.Size(338, 310);
+            this.richTextRecibido.TabIndex = 2;
+            this.richTextRecibido.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "En Preparación";
+            // 
+            // richTxtEnvio
+            // 
+            this.richTxtEnvio.AutoSize = true;
+            this.richTxtEnvio.Location = new System.Drawing.Point(788, 43);
+            this.richTxtEnvio.Name = "richTxtEnvio";
+            this.richTxtEnvio.Size = new System.Drawing.Size(60, 17);
+            this.richTxtEnvio.TabIndex = 4;
+            this.richTxtEnvio.Text = "En Viaje";
+            // 
+            // richTxtEntregado
+            // 
+            this.richTxtEntregado.AutoSize = true;
+            this.richTxtEntregado.Location = new System.Drawing.Point(1307, 43);
+            this.richTxtEntregado.Name = "richTxtEntregado";
+            this.richTxtEntregado.Size = new System.Drawing.Size(74, 17);
+            this.richTxtEntregado.TabIndex = 5;
+            this.richTxtEntregado.Text = "Entregado";
+            // 
+            // lblRetiro
+            // 
+            this.lblRetiro.AutoSize = true;
+            this.lblRetiro.Location = new System.Drawing.Point(1020, 449);
+            this.lblRetiro.Name = "lblRetiro";
+            this.lblRetiro.Size = new System.Drawing.Size(113, 17);
+            this.lblRetiro.TabIndex = 9;
+            this.lblRetiro.Text = "Listo para retirar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(485, 449);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "En Preparación";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(902, 480);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(338, 310);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBoxPrepRetiro
+            // 
+            this.richTextBoxPrepRetiro.Location = new System.Drawing.Point(366, 480);
+            this.richTextBoxPrepRetiro.Name = "richTextBoxPrepRetiro";
+            this.richTextBoxPrepRetiro.Size = new System.Drawing.Size(338, 310);
+            this.richTextBoxPrepRetiro.TabIndex = 6;
+            this.richTextBoxPrepRetiro.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pedidoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1598, 28);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pedidoToolStripMenuItem
+            // 
+            this.pedidoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarToolStripMenuItem});
+            this.pedidoToolStripMenuItem.Name = "pedidoToolStripMenuItem";
+            this.pedidoToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.pedidoToolStripMenuItem.Text = "Pedido";
+            // 
+            // agregarToolStripMenuItem
+            // 
+            this.agregarToolStripMenuItem.Name = "agregarToolStripMenuItem";
+            this.agregarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.agregarToolStripMenuItem.Text = "Agregar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1307, 449);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Número de pedido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1309, 483);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Acá agrupar los id";
+            // 
+            // FormPpal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1598, 833);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblRetiro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBoxPrepRetiro);
+            this.Controls.Add(this.richTxtEntregado);
+            this.Controls.Add(this.richTxtEnvio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextRecibido);
+            this.Controls.Add(this.richTextDelivery);
+            this.Controls.Add(this.richTxtPrepEnvio);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormPpal";
+            this.Text = "FormEnvios";
+            this.Load += new System.EventHandler(this.FormEnvios_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTxtPrepEnvio;
+        private System.Windows.Forms.RichTextBox richTextDelivery;
+        private System.Windows.Forms.RichTextBox richTextRecibido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label richTxtEnvio;
+        private System.Windows.Forms.Label richTxtEntregado;
+        private System.Windows.Forms.Label lblRetiro;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxPrepRetiro;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pedidoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
--- 2Parcial/FormEnvios/Program.cs
+++ 2Parcial/FormEnvios/Program.cs
@@ -16,7 +16,7 @@ namespace FormEnvios
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormPpal());
         }
     }
 }
